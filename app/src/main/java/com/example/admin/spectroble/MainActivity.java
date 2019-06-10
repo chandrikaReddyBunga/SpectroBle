6e42c2b03b2539dc2cf1e9eb5cc09a07c537e73a
chandrikaReddy Bunga <chandrika421@gmail.com>
2019-06-10 16:29:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/spectroble/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.spectroble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.spectrumsdk.FRAMEWORK.SCConnectionHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
